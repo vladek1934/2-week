6203c85fd455300a6d504073672f077671f532bf
vladek1934 <vlad.mun1999@gmail.com>
2017-01-30 12:45:40 +0600

    lab 2

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    { 
+       static string prost(string a)
+        {
+            string line = System.IO.File.ReadAllText(a);
+            string[] param = line.Split(' ');
+            int prostoe = 1000;
+                int m = 0;
+                for (int i = 0; i < param.Length; i++)
+                {
+
+                    if (int.Parse(param[i]) > 0)
+                    {
+                        m = 0;
+                        for (int h = 2; h < int.Parse(param[i]); h++)
+                        {
+                            if (int.Parse(param[i]) % h == 0) { m = 1; }
+                        }
+                    }
+                    if ((m == 0) && (int.Parse(param[i]) <= prostoe)) { prostoe = int.Parse(param[i]); }
+                    if (int.Parse(param[i]) == 1) { prostoe = 1; }
+                    if ((int.Parse(param[i]) == 2) && (prostoe >= 2)) { prostoe = 2; }
+                    
+                }
+                return prostoe.ToString();
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            string line = System.IO.File.ReadAllText(@"C:\Users\v_mun\kek.txt");
+            string[] param = line.Split(' ');
+            int max = int.Parse(param[0]);
+            int min = int.Parse(param[0]);
+            if (param.Length > 1)
+            {
+                for (int i = 0; i < param.Length - 1; i++)
+                {
+                    int b = int.Parse(param[i]);
+                    int c = int.Parse(param[i + 1]);
+                    if ((b >= c) && (b >= max)) { max = b; }
+                    else
+                    {
+                        if ((b <= c) && (c >= max)) { max = c; }
+                        
+                    }
+                }
+                for (int i = 0; i < param.Length - 1; i++)
+                {
+                    int b = int.Parse(param[i]);
+                    int c = int.Parse(param[i + 1]);
+                    if ((b <= c) && (b <= min)) { min = b; }
+                    else
+                    {
+                        if ((c <= b) && (c <= min)) { min = c; }
+                        
+                    }
+                }
+
+                string s= @"C:\Users\v_mun\kek.txt";
+                string prostoe = prost(s);
+
+                /*int prostoe = 1000;
+                int m = 0;
+                for (int i = 0; i < param.Length; i++)
+                {
+
+                    if (int.Parse(param[i]) > 0)
+                    {
+                        m = 0;
+                        for (int h = 2; h < int.Parse(param[i]); h++)
+                        {
+                            if (int.Parse(param[i]) % h == 0) { m = 1; }
+                        }
+                    }
+                    if ((m == 0) && (int.Parse(param[i]) <= prostoe)) { prostoe = int.Parse(param[i]); }
+                    if (int.Parse(param[i]) == 1) { prostoe = 1; }
+                    if ((int.Parse(param[i]) == 2) && (prostoe >= 2)) { prostoe = 2; }
+                }*/
+
+                Console.WriteLine("max = " + max);
+                Console.WriteLine("min = " + min);
+                Console.WriteLine("prostoe = " + prostoe);
+                System.IO.File.WriteAllText(@"C:\Users\v_mun\kek1.txt", prostoe);
+            }
+            
+        }
+    }
+}
